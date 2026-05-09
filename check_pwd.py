841555bc07354fd548f5c3f97bf2d047ed565373
angeletapersaud <angejohnson90@yahoo.com>
2026-05-09 19:48:13 -0400

    check for a length that is less than 8

--- check_pwd.py
+++ check_pwd.py
@@ -2,10 +2,11 @@
 
 symbols = "~`!@#$%^&*()_+-="
 
-# must be between 8-20 characters long and have one of each: lowercase letter, uppercase letter, integer & symbol
+
 def check_pwd(password: str) -> bool:
-	
-	if len(password) < 8 or len(password) > 20:
-		return True
+	if len(password) < 8 :
+		return False
+	return True
+    
 
 	
