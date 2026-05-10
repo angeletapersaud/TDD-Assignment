62157da336403708c96f2d3ac15047f80fa57590
angeletapersaud <angejohnson90@yahoo.com>
2026-05-09 20:18:38 -0400

    test for fail on all uppercase

--- check_pwd.py
+++ check_pwd.py
@@ -6,7 +6,8 @@ symbols = "~`!@#$%^&*()_+-="
 def check_pwd(password: str) -> bool:
 	if len(password) < 8 or len(password) > 20:
 		return False
-
+	if not any(character.islower() for character in password):
+		return False
 	return True
     
 
